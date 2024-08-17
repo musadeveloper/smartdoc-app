8c382adca81f8d6a448f8c09b43e99c674ab5d2d
Musawenkosi <musadeveloper07@gmail.com>
2024-08-17 19:14:01 +0200

    Added styling to the login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -32,25 +32,56 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
+      /*appBar: AppBar(title: Text('Login')),*/
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // App Logo and Name
+            Image.asset('assets/images/vecteezy_3d-doctor-character-presenting-and-lying-on-big-empty-phone_36485009.png', height: 170), // Adjust height as needed
+            SizedBox(height: 16),
+            Text(
+              'SmartDoc',
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF5DB8AE), // Color for the "SmartDoc" text
+              ),
+            ),
+            SizedBox(height: 15),
+            // Email Field
             TextField(
               controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
               keyboardType: TextInputType.emailAddress,
             ),
+            SizedBox(height: 16),
+            // Password Field
             TextField(
               controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
               obscureText: true,
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 32),
+            // Custom Login Button
             ElevatedButton(
               onPressed: _login,
               child: Text('Login'),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white, backgroundColor: Color(0xFF5DB8AE), // Text color
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12), // Adjust the radius as needed
+                ),
+              ),
             ),
           ],
         ),
